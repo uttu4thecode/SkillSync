2440973045a0f6e50250eb8eace90b19382ec2e2
uttu4thecode <www.utkarshkanha@gmail.com>
2026-03-25 12:08:39 +0530

    Add static css style.css

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app():
-    app = Flask(__name__, static_folder='static', static_url_path='/static')
+    app = Flask(__name__)
     app.config.from_object(Config)
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
