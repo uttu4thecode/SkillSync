f7eb44683442e5994c849b3ae990d37d217c5b25
uttu4thecode <www.utkarshkanha@gmail.com>
2026-03-25 10:02:52 +0530

    Fix caching issue for static files

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,9 @@ db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static', static_url_path='/static')
     app.config.from_object(Config)
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
     db.init_app(app)
     jwt.init_app(app)
